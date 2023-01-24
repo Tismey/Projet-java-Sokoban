d901247511844349b12853ef546a90edcf83d7d3
Tismey <108024662+Tismey@users.noreply.github.com>
2023-01-24 15:33:45 +0100

    Une interface et deux enums pour commencer le reste g eu la flm

---
+++ Cells.java
@@ -0,0 +1,17 @@
+/*
+ * Enum pour stocker les niveaux au runtime (representation des niveaux avec un double tableau
+ * d enum)
+ * 
+ */
+
+
+public enum Cells {
+
+    SOL,
+    MUR,
+    BOITE,
+    JOUEUR,
+    NIVEAUX,
+    SORTIE,
+
+}
---
+++ Memory.java
@@ -0,0 +1,25 @@
+public interface Memory{
+    /*
+     * A changer a votre discretion ( remplacer path par un class qui encapsule tout le fichier je l'est connais pas :/)
+     * interface pour la partie memoire
+     * 
+     * Note : Cette class pourait etre abstract pour avoir une autre classe niveaux
+     */
+    public Cells[][] loadFromFile(String path);
+
+    /*
+     * Interessant pour la recurence(niveaux dans des niveaux)
+     */
+
+    public Cells[][] rLoadFromFile(String path, int depth);
+
+     /*
+      * Savoir combien de niveaux dans ce niveaux
+      */
+
+    public int depthLevel(String path);
+
+    /*
+     *  ajout possible
+     */
+}
