fcf16701aabbae81797ab95a04b67d41f70c4872
Lyes-P <123511675+Lyes-P@users.noreply.github.com>
2023-02-25 16:10:08 +0100

    Classe pour les coordonnées

---
+++ CoordSet.java
@@ -0,0 +1,35 @@
+public class CoordSet {
+	private int x;
+	private int y;
+
+	public CoordSet (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return this.x;
+	}
+
+    public int getY() {
+    	return this.y;
+    }
+
+    public void addToX(int p) {
+    	this.x += p;
+    }
+    
+    public void addToY(int p) {
+    	this.y += p;
+    }
+
+    public boolean equals(Object o) {
+    	if (!(o instanceof CoordSet)) {
+    		return false;
+    	}
+
+    	CoordSet autre = (CoordSet) o;
+
+    	return ((getX() == autre.getX()) && (getY() == autre.getY()));
+    }
+}
