1a1a607a50c52f26e7bce1411d08b9f36bdf4143
Yanis-Ben-Kaoudjt <123511611+Yanis-Ben-Kaoudjt@users.noreply.github.com>
2023-03-20 18:23:10 +0100

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Frame{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
