412190961db617310ab208c8dc156b126b861e8d
Tismey <108024662+Tismey@users.noreply.github.com>
2023-03-29 20:36:08 +0200

    graphique recursif

--- Display.java
+++ Display.java
@@ -12,6 +12,7 @@ public class Display extends JPanel {
     private Frame frame;
     private LevelMove l;
     private LevelData d;
+    private  Univers u;
     private ArrayList<Image> images;
 
     public static void main(String[] args) throws IOException {
@@ -34,6 +35,23 @@ public class Display extends JPanel {
         images.add(ImageIO.read(new File("target.png")));
     }
 
+    public Display(Univers univ) throws IOException {
+        u = univ;
+        l = univ.getUnivers().get(univ.getPlayerSpawnWorld());
+        
+        images = new ArrayList<>();
+        images.add(ImageIO.read(new File("box.png")));
+        images.add(ImageIO.read(new File("wall.jpg")));
+        images.add(ImageIO.read(new File("player.jpg")));
+        images.add(ImageIO.read(new File("target.png")));
+    }
+
+    
+
+    
+
+    
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.setBackground(Color.DARK_GRAY);
@@ -47,6 +65,22 @@ public class Display extends JPanel {
                     g.drawImage(images.get(2), j * images.get(2).getWidth(null), i * images.get(2).getHeight(null), null);
                 else if (l.getListTarget().contains(new CoordSet(i, j)))
                     g.drawImage(images.get(3), j * images.get(3).getWidth(null), i * images.get(3).getHeight(null), null);
+                if (l.getLevelData(i, j) >= 0){
+                    LevelMove l2 = u.getUnivers().get(l.getLevelData(i, j));
+                    for (int t = 0; t < l2.getSizeMat();t++) {
+                        for (int r = 0; r < l2.getSizeMat(); r++) {
+                            if (l2.getLevelData(i, j) == Cells.BOITE)
+                                g.drawImage(images.get(0), j*images.get(0).getWidth(null) + r*images.get(0).getWidth(null)/l2.getSizeMat(), i * images.get(0).getHeight(null) + t*images.get(0).getHeight(null)/l2.getSizeMat(),images.get(0).getWidth(null)/l2.getSizeMat(),images.get(0).getHeight(null)/l2.getSizeMat(),null);
+                            else if (l.getLevelData(i, j) == Cells.MUR)
+                                g.drawImage(images.get(1), j*images.get(1).getWidth(null) + r*images.get(1).getWidth(null)/l2.getSizeMat(), i * images.get(1).getHeight(null) + t*images.get(1).getHeight(null)/l2.getSizeMat(),images.get(1).getWidth(null)/l2.getSizeMat(),images.get(1).getHeight(null)/l2.getSizeMat(),null);
+                            else if (l.getLevelData(i, j) == Cells.JOUEUR)
+                                g.drawImage(images.get(2), j*images.get(2).getWidth(null) + r*images.get(2).getWidth(null)/l2.getSizeMat(), i * images.get(2).getHeight(null) + t*images.get(2).getHeight(null)/l2.getSizeMat(),images.get(2).getWidth(null)/l2.getSizeMat(),images.get(2).getHeight(null)/l2.getSizeMat(),null);
+                            else if (l.getListTarget().contains(new CoordSet(i, j)))
+                                g.drawImage(images.get(3), j*images.get(3).getWidth(null) + r*images.get(3).getWidth(null)/l2.getSizeMat(), i * images.get(3).getHeight(null) + t*images.get(3).getHeight(null)/l2.getSizeMat(),images.get(3).getWidth(null)/l2.getSizeMat(),images.get(3).getHeight(null)/l2.getSizeMat(),null);
+                        }
+                    }
+                }
+                   
             }
         }
     }
