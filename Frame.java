cbd1505b37221af57a0e577bcdcc0c8c307adad5
Yanis-Ben-Kaoudjt <123511611+Yanis-Ben-Kaoudjt@users.noreply.github.com>
2023-03-29 20:54:52 +0200

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
-import java.util.*;
-import java.io.IOException;
-import java.awt.*;
+import java.io.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -10,141 +8,160 @@ import java.awt.event.KeyListener;
 public class Frame extends JFrame implements KeyListener{
     private Display display;
     private LevelMove l;
-    private LevelData d;
 
-    public Frame(){
+    public Frame() throws IOException{
         super();
+        display = new Display();
         l = new LevelMove(1);
-        d = new LevelData(getName());
         add(display);
         addKeyListener(this);
         setSize(1920, 1080);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-    public Frame(LevelMove level, LevelData data) throws IOException{
+    public Frame(LevelMove level) throws IOException{
         super();
-        display = new Display(level, data);
+        display = new Display(level);
         l = level;
-        d = data;
         add(display);
         this.addKeyListener(new KeyAdapter() {
             public void keyTyped(KeyEvent e){
+                if(e.getKeyCode() != KeyEvent.VK_DOWN && e.getKeyCode() != KeyEvent.VK_UP && e.getKeyCode() != KeyEvent.VK_RIGHT && e.getKeyCode() != KeyEvent.VK_LEFT)
+                    System.out.println("Mauvaise touche pressée, les mouvements se font avec les touches directionnelles.");
+            };
+        
+            public void keyPressed(KeyEvent e){
                 switch(e.getKeyCode()){
                     case KeyEvent.VK_LEFT: 
                         if (l.checkForMove(l.playerSpawn(), Direction.GAUCHE))
                             l.move(l.playerSpawn(), Direction.GAUCHE);
-                        System.out.println("test");
                         display.maj();
+                        if (l.winConditionMet()){
+                            System.out.println("Niveau réussi !\nFin du jeu");
+                            System.exit(0);
+                        }
                         break;
                     case KeyEvent.VK_RIGHT: 
                         if (l.checkForMove(l.playerSpawn(), Direction.DROITE))
                             l.move(l.playerSpawn(), Direction.DROITE);
                         display.maj();
+                        if (l.winConditionMet()){
+                            System.out.println("Niveau réussi !\nFin du jeu");
+                            System.exit(0);
+                        }
                         break;
                     case KeyEvent.VK_UP: 
                         if (l.checkForMove(l.playerSpawn(), Direction.HAUT))
                             l.move(l.playerSpawn(), Direction.HAUT);
                         display.maj();
+                        if (l.winConditionMet()){
+                            System.out.println("Niveau réussi !\nFin du jeu");
+                            System.exit(0);
+                        }
                         break;
                     case KeyEvent.VK_DOWN: 
                         if (l.checkForMove(l.playerSpawn(), Direction.BAS))
                             l.move(l.playerSpawn(), Direction.BAS);
                         display.maj();
+                        if (l.winConditionMet()){
+                            System.out.println("Niveau réussi !\nFin du jeu");
+                            System.exit(0);
+                        }
                         break;
                 }
             };
         
-            public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode()==KeyEvent.KEY_PRESSED)
-                    System.out.println("Key pressed");
-            };
-        
             public void keyReleased(KeyEvent e) {
-                if(e.getKeyCode()==KeyEvent.KEY_RELEASED)
-                    System.out.println("Key released");
-            };
+                switch(e.getKeyCode()){
+                    case KeyEvent.VK_LEFT: 
+                        if (l.checkForMove(l.playerSpawn(), Direction.GAUCHE))
+                            System.out.println("Gauche");
+                        break;
+                    case KeyEvent.VK_RIGHT: 
+                        if (l.checkForMove(l.playerSpawn(), Direction.DROITE))
+                            System.out.println("Droite");
+                        break;
+                    case KeyEvent.VK_UP: 
+                        if (l.checkForMove(l.playerSpawn(), Direction.HAUT))
+                            System.out.println("Haut");
+                        break;
+                    case KeyEvent.VK_DOWN: 
+                        if (l.checkForMove(l.playerSpawn(), Direction.BAS))
+                            System.out.println("Bas");
+                        break;
+                }
+            }
         });
-        setSize(1000, 1000);
+        setSize(1920, 1080);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-    public boolean moveUp(KeyEvent e){
-        l = d.getListData().get(0);
-        
-        if(e.getKeyCode() == KeyEvent.VK_UP){
-            if (l.checkForMove(l.playerSpawn(), Direction.HAUT))
-                l.move(l.playerSpawn(), Direction.HAUT);
-            display.maj();
-        } 
-        return true;                    
-    }
 
-    public void moveDown(KeyEvent e){
-        l = d.getListData().get(0);
-        
-        if(e.getKeyCode() == KeyEvent.VK_UP){
-            if (l.checkForMove(l.playerSpawn(), Direction.BAS))
-                l.move(l.playerSpawn(), Direction.BAS);
-            display.maj();
-        }                     
-    }
-
-    public void moveRight(KeyEvent e){
-        l = d.getListData().get(0);
-        
-        if(e.getKeyCode() == KeyEvent.VK_UP){
-            if (l.checkForMove(l.playerSpawn(), Direction.DROITE))
-                l.move(l.playerSpawn(), Direction.DROITE);
-            display.maj();
-        }                     
-    }
-
-    public boolean moveLeft(KeyEvent e){
-        l = d.getListData().get(0);
-        
-        if(e.getKeyCode() == KeyEvent.VK_UP){
-            if (l.checkForMove(l.playerSpawn(), Direction.GAUCHE))
-                l.move(l.playerSpawn(), Direction.GAUCHE);
-            display.maj();
-        } 
-        return true;                    
-    }
-
-    @Override
     public void keyTyped(KeyEvent e){
+        if(e.getKeyCode() != KeyEvent.VK_DOWN && e.getKeyCode() != KeyEvent.VK_UP && e.getKeyCode() != KeyEvent.VK_RIGHT && e.getKeyCode() != KeyEvent.VK_LEFT)
+            System.out.println("Mauvaise touche pressée, les mouvements se font avec les touches directionnelles.");
+    }
+
+    public void keyPressed(KeyEvent e){
         switch(e.getKeyCode()){
             case KeyEvent.VK_LEFT: 
                 if (l.checkForMove(l.playerSpawn(), Direction.GAUCHE))
                     l.move(l.playerSpawn(), Direction.GAUCHE);
                 display.maj();
+                if (l.winConditionMet()){
+                    System.out.println("Niveau réussi !\nFin du jeu");
+                    System.exit(0);
+                }
                 break;
             case KeyEvent.VK_RIGHT: 
                 if (l.checkForMove(l.playerSpawn(), Direction.DROITE))
                     l.move(l.playerSpawn(), Direction.DROITE);
                 display.maj();
+                if (l.winConditionMet()){
+                    System.out.println("Niveau réussi !\nFin du jeu");
+                    System.exit(0);
+                }
                 break;
             case KeyEvent.VK_UP: 
                 if (l.checkForMove(l.playerSpawn(), Direction.HAUT))
                     l.move(l.playerSpawn(), Direction.HAUT);
                 display.maj();
+                if (l.winConditionMet()){
+                    System.out.println("Niveau réussi !\nFin du jeu");
+                    System.exit(0);
+                }
                 break;
             case KeyEvent.VK_DOWN: 
                 if (l.checkForMove(l.playerSpawn(), Direction.BAS))
                     l.move(l.playerSpawn(), Direction.BAS);
                 display.maj();
+                if (l.winConditionMet()){
+                    System.out.println("Niveau réussi !\nFin du jeu");
+                    System.exit(0);
+                }
                 break;
         }
     }
-
-    public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.KEY_PRESSED)
-            System.out.println("Key pressed");
-    }
-
+    
     public void keyReleased(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.KEY_RELEASED)
-            System.out.println("Key released");
+        switch(e.getKeyCode()){
+            case KeyEvent.VK_LEFT: 
+                if (l.checkForMove(l.playerSpawn(), Direction.GAUCHE))
+                    System.out.println("Gauche");
+                break;
+            case KeyEvent.VK_RIGHT: 
+                if (l.checkForMove(l.playerSpawn(), Direction.DROITE))
+                    System.out.println("Droite");
+                break;
+            case KeyEvent.VK_UP: 
+                if (l.checkForMove(l.playerSpawn(), Direction.HAUT))
+                    System.out.println("Haut");
+                break;
+            case KeyEvent.VK_DOWN: 
+                if (l.checkForMove(l.playerSpawn(), Direction.BAS))
+                    System.out.println("Bas");
+                break;
+        }
     }
 
     public Display getDisplay(){
